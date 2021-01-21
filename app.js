b22dade508a5c7fcf7d26f3e51157d1855dac068
Mayur C Dhage <mdhage139@altimetrik.com>
2021-01-21 14:56:52 +0530

    initialised new notes app

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs=require('fs')
+const chalk=require('chalk')
+let utils=require('./utils')
+let validator=require('validator')
+
+// console.log(utils)
+
+// console.log(utils.add(1,2))
+// console.log(validator.isEmail('mc@gmail.com'))
+
+// console.log(chalk.green('hello'))
+
+// fs.writeFileSync('notes.txt','this was crreated by me'+Date.now())
+
+console.log(process.argv)
---
+++ utils.js
@@ -0,0 +1,7 @@
+let name='mayur'
+
+let add=(x,y)=>x+y;
+
+console.log(name)
+
+module.exports={name,add}
